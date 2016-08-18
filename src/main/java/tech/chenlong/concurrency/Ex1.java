047c5a1f10f67de0dcdf23d6f10146c12f0fe987
longchen <chenlong_r@163.com>
2016-08-18 09:17:51 +0800

    add concurrency ex1

---
+++ src/main/java/tech/chenlong/concurrency/Ex1.java
@@ -0,0 +1,27 @@
+package tech.chenlong.concurrency;
+
+/**
+ * Created by longchen on 16-8-17
+ */
+public class Ex1 implements Runnable {
+
+
+    private Ex1(String name) {
+        System.out.println(name + "start");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(Thread.currentThread().getName() + "process");
+            Thread.yield();
+        }
+        System.out.println(Thread.currentThread().getName() + "stop");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Ex1("thread" + i), "thread" + i).start();
+        }
+    }
+}
---
+++ src/main/java/tech/chenlong/concurrency/LiftOff.java
@@ -0,0 +1,36 @@
+package tech.chenlong.concurrency;
+
+/**
+ * Created by longchen on 16-8-17
+ */
+public class LiftOff implements Runnable {
+    protected int countDown = 100;
+    private static int taskCount = 0;
+    private final int id = taskCount ++;
+
+    public LiftOff() {
+    }
+
+    public LiftOff(int countDown) {
+        this.countDown = countDown;
+    }
+
+    public String status() {
+        return "#" + id + "(" + (countDown > 0 ? countDown : "Liftoff!") + "), ";
+    }
+
+    @Override
+    public void run() {
+        while (countDown-- > 0) {
+            System.out.println(status());
+            Thread.yield();
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            new Thread(new LiftOff()).start();
+        }
+        System.out.println("Waiting for LiftOff");
+    }
+}
